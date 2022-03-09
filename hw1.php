92785e611e255ef0ec0a9cd7046e2cd37622ba14
ShariganXII <harimaru@Outlook.com>
2022-03-08 19:35:39 -0500

    Update hw1.php

--- hw1.php
+++ hw1.php
@@ -1,30 +1,27 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <?php
-            $name = "Junjie Du";
-            $credit = 11;
-            var_dump($name);
-            echo "<br>";
-            var_dump($credit);
-            echo "<br>";
-            echo "Welcome ", $name;
-            if($credit >= 12)
-                echo "Full Time Student";
-            else
-                echo "Part Time Student";
-            echo $_SERVER['PHP_SELF'];
-            echo "<br>";
-            echo $_SERVER['SERVER_NAME'];
-            echo "<br>";
-            echo $_SERVER['HTTP_HOST'];
-            echo "<br>";
-            echo $_SERVER['HTTP_REFERER'];
-            echo "<br>";
-            echo $_SERVER['HTTP_USER_AGENT'];
-            echo "<br>";
-        ?>
-    </body>
-</html>
+<?php
+$name = "Junjie Du";
+$credit = 11;
+var_dump($name);
+echo "<br>";
+var_dump($credit);
+echo "<br>";
+echo "Welcome ", $name;
+if($credit >= 12)
+    echo "Full Time Student";
+else
+    echo "Part Time Student";
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+?>
+
+
 
 
