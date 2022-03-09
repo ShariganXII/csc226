a45df65b8770af3fd7a9ed1fb8ddbd30d3ff881a
ShariganXII <harimaru@Outlook.com>
2022-03-08 20:00:32 -0500

    Update hw1.php

--- hw1.php
+++ hw1.php
@@ -1,22 +1,24 @@
 <?php
 $name = "Junjie Du";
 $credit = 11;
+$any_val = 5.5;
 var_dump($name);
 echo "<br>";
 var_dump($credit);
 echo "<br>";
-echo "Welcome ", $name;
+var_dump($any_val);
+echo "<br>";
+echo "Welcome ", $name; echo "<br>";
+
 if($credit >= 12)
     echo "Full Time Student";
 else
-    echo "Part Time Student";
+    echo "Part Time Student"; echo "<br>";
 echo $_SERVER['PHP_SELF'];
 echo "<br>";
 echo $_SERVER['SERVER_NAME'];
 echo "<br>";
 echo $_SERVER['HTTP_HOST'];
-echo "<br>";
-echo $_SERVER['HTTP_REFERER'];
 echo "<br>";
 echo $_SERVER['HTTP_USER_AGENT'];
 echo "<br>";
